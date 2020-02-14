a5f687abfc9e26bd46f1697ef5158ebcb144d646
Bruno Geisler Vigentas <bruno.vigentas@hbsis.com.br>
2020-02-14 08:30:40 -0300

    Created routes

---
+++ src/config/routes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+module.exports = function(server) {
+
+    //Sempre que URL inicia com /api direciona para o router
+    const router = express.Router()
+    server.use('/api', router)
+
+    //Cria rotas dos services
+    const BillingCycle = require('../api/billing-cycle/billingCycleService')
+    BillingCycle.register('router', '/billingCycles')
+
+}
